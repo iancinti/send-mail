18bb670ed6aaf0e8ad531c5fef9736a61872fd74
Ian Cinti <ian.cinti@gmail.com>
2024-03-03 18:19:22 -0300

    added SendEmailConfig

---
+++ src/main/kotlin/com/mail/send/config/SendEmailConfig.kt
@@ -0,0 +1,30 @@
+package com.mail.send.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
+
+@Configuration
+class emailConfig {
+
+    @Bean
+    fun getJavaMailSender(): JavaMailSender {
+        val mailSender = JavaMailSenderImpl()
+        mailSender.host = "smtp.gmail.com"
+        mailSender.port = 587
+
+        mailSender.username = "cclabsf@gmail.com"
+        mailSender.password = ""
+
+        val props = mailSender.javaMailProperties
+        props["mail.transport.protocol"] = "smtp"
+        props["mail.smtp.auth"] = "true"
+        props["mail.smtp.starttls.enable"] = "true"
+        props["mail.debug"] = "true"
+
+        return mailSender
+    }
+
+
+}
