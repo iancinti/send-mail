8c0dd0828829ab00eaf8fb24cbf704e605dd37e6
Ian Cinti <ian.cinti@gmail.com>
2024-03-03 18:22:59 -0300

    added several

---
+++ src/test/kotlin/com/mail/send/SendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mail.send
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
